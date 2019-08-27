1ce36b37472975136375b2b99229aef4140f8131
Kartik G <kartik.g@tallysolutions.com>
2019-08-27 15:41:31 +0530

    added angular material

--- frontend/mafiaApp/src/app/app-routing.module.ts
+++ frontend/mafiaApp/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WaitingAreaComponent } from './waiting-area/waiting-area.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {'path': '', component:LoginpageComponent },
+  {'path': 'lobby', component:WaitingAreaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/mafiaApp/src/app/app.module.ts
+++ frontend/mafiaApp/src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CountdownModule } from 'ngx-countdown';
 import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card'; 
+import {MatExpansionModule} from '@angular/material/expansion'; 
+import {MatListModule} from '@angular/material/list'; 
+
 import { AppComponent } from './app.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
 import { WaitingAreaComponent } from './waiting-area/waiting-area.component';
@@ -15,7 +21,12 @@ import { WaitingAreaComponent } from './waiting-area/waiting-area.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CountdownModule
+    CountdownModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
