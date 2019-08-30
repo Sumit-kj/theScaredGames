ea3f0c25682a7e0c25f57b5d1ba04aa0bb02c639
Ankit Raj <ankit.raj@tallysolutions.com>
2019-08-30 10:31:21 +0530

    card shuffle

--- frontend/mafiaApp/src/app/loginpage/loginpage.component.ts
+++ frontend/mafiaApp/src/app/loginpage/loginpage.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { UserSessionsService } from './../user-sessions.service';
 import { Component } from '@angular/core';
 @Component({
@@ -7,9 +8,14 @@ import { Component } from '@angular/core';
 })
 export class LoginpageComponent {
   username:string = "";
-  constructor(private userService:UserSessionsService) { }
-   
+  constructor(private userService:UserSessionsService,private http:HttpClient) { }
+  url:string ="http://localhost:8000/create_session/"; 
   startGame():void {
     this.userService.userName = this.username;
+    var json_name = new FormData();
+    json_name.append('name',this.username);
+    this.http.post(this.url,json_name,{'responseType':'json'}).subscribe(response=>
+      {console.log(response);}
+      );
   }
 }
--- frontend/mafiaApp/src/app/waiting-area/waiting-area.component.ts
+++ frontend/mafiaApp/src/app/waiting-area/waiting-area.component.ts
@@ -12,8 +12,18 @@ import { timer } from 'rxjs';
 export class WaitingAreaComponent implements OnInit {
   user:User = new User();
   visibleLink=false;
+  userRole ="mafia";
   cardImage ="https://media.istockphoto.com/photos/playing-card-king-of-spades-picture-id458126511?k=6&m=458126511&s=612x612&w=0&h=x0PjZz2iHWp20B02idcOE_UBoOh2XGkQoUiucxcHalg=";
-  imageList =["https://cdn.pixabay.com/photo/2013/07/13/13/45/playing-card-161489__480.png","https://cdn.pixabay.com/photo/2013/07/13/13/45/playing-card-161485__480.png","https://cdn.pixabay.com/photo/2013/07/13/13/45/playing-card-161488__480.png"];
+  imageList =["https://cdn.pixabay.com/photo/2013/07/13/13/45/playing-card-161489__480.png","https://cdn.pixabay.com/photo/2013/07/13/13/45/playing-card-161485__480.png",
+  "https://cdn.pixabay.com/photo/2014/03/25/17/01/spades-297839__480.png",
+  "https://cdn.pixabay.com/photo/2013/07/13/13/45/playing-card-161488__480.png",
+  "https://cdn.pixabay.com/photo/2013/07/13/13/46/playing-card-161494_1280.png",
+  "https://cdn.pixabay.com/photo/2012/04/18/19/20/card-37623_1280.png",
+"https://cdn.pixabay.com/photo/2013/07/13/13/45/playing-card-161487__480.png",
+"https://cdn.pixabay.com/photo/2013/07/13/13/45/playing-card-161490__480.png",
+"https://cdn.pixabay.com/photo/2013/07/13/13/46/playing-card-161492__480.png",
+"https://cdn.pixabay.com/photo/2013/07/13/13/45/playing-card-161487__480.png",
+];
   previousElement:any = undefined;
   constructor(private userService:UserSessionsService) { 
     this.visibleLink=false;
@@ -24,35 +34,14 @@ export class WaitingAreaComponent implements OnInit {
     this.visibleLink=!this.visibleLink;
   }
   i = 0;
-  animDuration = 50;
-  ntimes=0;
+  
   ngOnInit() {
     this.user.username = this.userService.User.username; 
-    this.user.avatar = "https://api.adorable.io/avatars/100/abe@adorable.png";
-    console.log(this.userService.User.username);
-    
-    var timer = setInterval(()=>{
-      console.log(this.imageList[this.i]);
-      this.cardImage = this.imageList[this.i];
-      if(this.i == this.imageList.length-1)
-      {
-        this.i = 0;
-        this.ntimes++;
-      }
-      else {
-        this.i++;
-      }
-      if(this.ntimes == 15)
-      {
-        this.animDuration = 1000;
-        this.ntimes++;
-      }
-      else if(this.ntimes == 30){
-          clearInterval(timer);
-      }
-
-    }, this.animDuration);  
+    this.user.avatar = "https://api.adorable.io/avatars/100/abe@adorable.png";    
+    this.setCards(50,0);
+    this.setCards(100,0);
   }
+
   setAvatar(element):void {
     console.log(element);
     if(this.previousElement !== undefined)
@@ -64,4 +53,36 @@ export class WaitingAreaComponent implements OnInit {
   beginGame():void {
     this.userService.setUserRole();
   }
+  setCards(val:number,times:number):void {
+    console.log("here");
+    var timer = setInterval(()=>{
+      // console.log(times);
+      this.cardImage = this.imageList[this.i];
+      if(this.i == this.imageList.length-1)
+      {
+        this.i = 0;
+        times++;
+      }
+      else if(times == 5){
+        var val =0;
+        switch(this.userRole){
+          case "citizen":
+            val = 4;break;
+          case "mafia":
+            val = 2;break;
+          case "doctor":
+            val = 3; break;
+          default:
+            val = 1;
+        }
+        this.cardImage = this.imageList[val];
+        clearInterval(timer);
+    }
+      else  {
+        this.i++;
+      }    
+    }, val);
+
+  }
+
 }
