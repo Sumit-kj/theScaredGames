76d791eb8792f6fca0aa101b5ab671abc13257b5
Sumit Kumar <sumit.k@tallysolutions.com>
2019-09-04 12:46:51 +0530

    join game module done

--- frontend/mafiaApp/src/app/loginpage/loginpage.component.ts
+++ frontend/mafiaApp/src/app/loginpage/loginpage.component.ts
@@ -11,6 +11,9 @@ import { from } from 'rxjs';
 })
 export class LoginpageComponent {
   username:string = "";
+  session:string = "";
+  isAlready:boolean = true;
+  isJoining:boolean = false;
   constructor(private userService:UserSessionsService,private http:HttpClient, private service:SyncService) { }
   url:string ="http://localhost:8000/create_session/"; 
   startGame():void {
@@ -18,4 +21,13 @@ export class LoginpageComponent {
     // console.log(this.userService.userName);
     this.service.startGame(this.username);
 }
+
+  joinGame():void{
+    this.isJoining = true;
+  }
+
+  enterGame():void{
+    this.userService.userName = this.username;
+    this.isAlready = this.service.joinGame(this.username,this.session);
+  }
 }
--- main/views.py
+++ main/views.py
@@ -35,12 +35,19 @@ def create_session(request):
         return json_error(e)
     return None
 
-
-def join_session(request, session):
+@csrf_exempt
+def join_session(request,session):
     try:
-        if Session.objects.filter(session=session).count() != 0:
+        player_session = Session.objects.get(session=session)
+        if Player.objects.get(session=player_session,name=request.POST['name']) is not None:
+            raise Exception('Player Already Exists!')
+            print("here")
+        elif Session.objects.get(session=str(session)) is not None:
             response = JsonResponse({'session': session, 'state': 'begin'})
-            response.set_cookie(session)
+            Player(name=request.POST['name'], alive=True,
+                   avatar="null", role="null", color="null", session=Session.objects.get(session=session)).save()
+            response.set_cookie('session', session)
+            return response
         else:
             raise Exception('session does not exist')
     except Exception as e:
@@ -54,7 +61,7 @@ def get_user(request):
 def create_player(request):
     try:
         if request.method == 'POST':
-            p = Player.objects.get(name=request.POST['name'])
+            p = Player.objects.get(name=request.POST['name'],session=request.POST['session'])
             p.alive = request.POST['alive']
             p.avatar = request.POST['avatar']
             p.color = request.POST['color']
@@ -106,4 +113,4 @@ def get_chat(request):
 
 
 def json_error(message):
-    return JsonResponse({'status': 'failed', 'error': message})
+    return JsonResponse({'status': 'failed', 'error': str(message)})
